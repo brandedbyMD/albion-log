2809e2eee7d4c0651385fbdf532a801b13aeff6d
brandedbyMD <mondaybranded1stfxxk@gmail.com>
2025-12-27 23:19:25 +0900

    Add files via upload

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -7,6 +7,12 @@ const SERVERS = {
   'asia': 'https://gameinfo-sgp.albiononline.com'
 };
 
+const MURDERLEDGER_SERVERS = {
+  'americas': 'https://murderledger.albiononline2d.com',
+  'europe': 'https://murderledger-europe.albiononline2d.com',
+  'asia': 'https://murderledger-asia.albiononline2d.com'
+};
+
 export async function onRequest(context) {
   const { request, params } = context;
   
@@ -20,16 +26,26 @@ export async function onRequest(context) {
   // Remove server param from the forwarded URL
   url.searchParams.delete('server');
   
-  // Build target URL
-  const baseUrl = SERVERS[server] || SERVERS['americas'];
-  const targetPath = `/api/gameinfo/${path}`;
-  const queryString = url.searchParams.toString();
-  const targetUrl = `${baseUrl}${targetPath}${queryString ? '?' + queryString : ''}`;
+  let targetUrl;
   
-  console.log('Proxying to:', targetUrl);
+  // Check if this is a Murder Ledger request
+  if (path.startsWith('murderledger/')) {
+    const mlPath = path.replace('murderledger/', '');
+    const baseUrl = MURDERLEDGER_SERVERS[server] || MURDERLEDGER_SERVERS['americas'];
+    const queryString = url.searchParams.toString();
+    targetUrl = `${baseUrl}/api/${mlPath}${queryString ? '?' + queryString : ''}`;
+    console.log('Proxying to Murder Ledger:', targetUrl);
+  } else {
+    // Build target URL for official Albion API
+    const baseUrl = SERVERS[server] || SERVERS['americas'];
+    const targetPath = `/api/gameinfo/${path}`;
+    const queryString = url.searchParams.toString();
+    targetUrl = `${baseUrl}${targetPath}${queryString ? '?' + queryString : ''}`;
+    console.log('Proxying to Albion API:', targetUrl);
+  }
   
   try {
-    // Forward the request to Albion API
+    // Forward the request
     const response = await fetch(targetUrl, {
       method: request.method,
       headers: {
